6f4ccaddf5978813387ea549bd1978166bdbd5c9
kmh5038 <144766297+kmh5038@users.noreply.github.com>
2023-12-26 10:10:00 +0000

    NavigationStack 추가
    NavigationStack 관련 코드 추가

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -10,11 +10,22 @@ import SwiftUI
 struct ScrumsView: View {
     let scrums: [DailyScrum]
     var body: some View {
-        List(scrums) { scrum in // DailyScrum 배열을 불러올건데 그 키값으로 .title을 사용 -> Identifiable 프로토콜을 채용해서 UUID로 식별자 변경
-            CardView(scrum: scrum) // 각 리스트를 CardView로 구성
+        NavigationStack {
+            List(scrums) { scrum in // DailyScrum 배열을 불러올건데 그 키값으로 .title을 사용 -> Identifiable 프로토콜을 채용해서 UUID로 식별자 변경
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum) // 각 리스트를 CardView로 구성
+                }
                 .listRowBackground(scrum.theme.mainColor) // 각 리스트를 불러온 DailyScrum 배열요소에 지정된 theme의 색을 mainColor로 접근하여 색을 입힌다
-            
+            }
+            .navigationTitle("Daily Scrums") // 네비게이션 타이틀 작성
+            .toolbar { // 툴바에 + 버튼 추가
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum") // 보이스오버 
+            }
         }
+        
     }
 }
 
@@ -23,3 +34,5 @@ struct ScrumsView_Previews: PreviewProvider {
         ScrumsView(scrums: DailyScrum.sampleData)
     }
 }
+
+
